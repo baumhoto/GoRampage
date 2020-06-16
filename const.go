aa04cf5ec2cfa0e99d003b9214d933dd60a04e0a
baumhoto <baumhoto@gmail.com>
2020-06-16 17:06:38 +0200

    further refactorings chapter 1

---
+++ const.go
@@ -0,0 +1,13 @@
+package main
+
+import "image/color"
+
+var (
+	clear = color.RGBA{0, 0, 0, 0}
+	black = color.RGBA{0, 0, 0, 255}
+	white = color.RGBA{255, 255, 255, 255}
+	gray  = color.RGBA{192, 192, 192, 255}
+	red   = color.RGBA{255, 0, 0, 255}
+	green = color.RGBA{0, 255, 0, 255}
+	blue  = color.RGBA{0, 0, 255, 255}
+)
--- vector.go
+++ vector.go
@@ -23,6 +23,12 @@ func (v *Vector) Multiply(multiplier float64) {
 	v.y *= multiplier
 }
 
+// Divide Vector
+func (v *Vector) Divide(divisor float64) {
+	v.x /= divisor
+	v.y /= divisor
+}
+
 // AddVectors adds 2 Vectors returning a new Vector
 func AddVectors(a Vector, b Vector) Vector {
 	return Vector{a.x + b.x, a.y + b.y}
@@ -36,4 +42,9 @@ func SubstractVectors(a Vector, b Vector) Vector {
 // MultiplyVector multiplies a Vector with a multiplier
 func MultiplyVector(a Vector, multiplier float64) Vector {
 	return Vector{a.x * multiplier, a.y * multiplier}
+}
+
+//  DivideVector divides a Vector with a divisor
+func DivideVector(a Vector, divisor float64) Vector {
+	return Vector{a.x / divisor, a.y / divisor}
 }
